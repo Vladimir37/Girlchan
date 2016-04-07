fa13dc6171ab5ca6622775832db09412fe6c55b5
Vladimir37 <vladimir_zapas@mail.ru>
2016-04-07 23:09:00 +0000

    Board controller constructor

---
+++ app/controllers/board.js
@@ -0,0 +1,30 @@
+var models = require('../basis/models');
+
+class BoardController {
+    constructor() {
+        var self = this;
+        var boards_data = {};
+        var lang_req = models.Lang.find();
+        var board_req = models.Board.find();
+        Promise.all([lang_req, board_req]).then(function(data) {
+            data[0].forEach(function(lang) {
+                boards_data[lang.addr] = {
+                    name: lang.name,
+                    boards: {}
+                };
+                data[1].forEach(function(board) {
+                    boards_data[lang.addr].boards[board.addr] = board.names[lang.addr];
+                });
+            });
+            self.boards = boards_data;
+            console.log('Boards data successfully created');
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
+    index(req, res, next) {
+        //
+    }
+}
+
+module.exports = BoardController;
--- app/router/board.js
+++ app/router/board.js
@@ -1,5 +1,7 @@
 var express = require('express');
 
+var BoardController = require('../controllers/board');
+
 var router = express.Router();
 
 module.exports = router;
