41b19b54b513bfa584702392fa1ebb4955a59cb6
Vladimir37 <vladimir_zapas@mail.ru>
2016-04-12 19:00:22 +0000

    Color circles 1.1

--- girlchan.js
+++ girlchan.js
@@ -4,7 +4,7 @@ var app = require('./app/app');
 var config = require('./config');
 
 // Front-end bundle
-var webpack = require('./app/basis/webpack');
-webpack();
+//var webpack = require('./app/basis/webpack');
+//webpack();
 
 http.createServer(app).listen(config.port_main);
